d728cb10b418b7538d49bf5d7d3d683bceab3896
AEH <117328917+Abdallahelhamawi@users.noreply.github.com>
2024-10-15 19:10:44 -0700

    Update load.py

--- PROTEC17 QA/load.py
+++ PROTEC17 QA/load.py
@@ -8,6 +8,7 @@ from langchain_community.document_loaders import PyPDFLoader
 import os
 import chromadb
 
+
 def load_chunk_persist_pdf() -> Chroma:
     OPENAI_API_KEY=""
     pdf_folder_path =os.path.abspath("C:\\Users\\abdal\\OneDrive\\Desktop\\PROTEC17 QA\\Contracts")
